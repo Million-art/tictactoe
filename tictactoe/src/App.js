5427c4af1ed7ba0d3577d35ae6b905a8bb9e2702
million-art <millionmulugeta09@gmail.com' git config --global user.name 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 git config --global user.email "millionmulugeta09@gmail.com>
2022-11-05 13:32:44 +0300

    creating components

---
+++ tictactoe/src/App.js
@@ -0,0 +1,14 @@
+ import './App.css';
+import Bored from './component/Board.js'
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1>TICTACTOE</h1>
+        <Bored />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tictactoe/src/component/Board.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Board.css';
+function Board(props) {
+  return (
+    <div className='board'>
+        <div className='board-row'>
+            <button>1</button>
+            <button>1</button>
+            <button>1</button>
+
+        </div>
+        <div className='board-row'>
+            <button>1</button>
+            <button>1</button>
+            <button>1</button>
+
+        </div>
+        <div className='board-row'>
+            <button>1</button>
+            <button>1</button>
+            <button>1</button>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Board
